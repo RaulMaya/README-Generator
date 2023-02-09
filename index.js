52856f72c4b353e3188a0a501a1165a181974c8b
Raul <raulmayas20@gmail.com>
2023-02-09 10:01:10 -0600

    Creating index.js, importing useful libraries, creating test file

---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+console.log("Creating README.md")
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
